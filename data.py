954c706eccae05d581f4648b6cdf715fb9c826c5
Zhuo Chen <zchenpds@gmail.com>
2018-03-03 20:44:59 -0500

    Change training data format

--- data.py
+++ data.py
@@ -10,26 +10,26 @@ import queue
 
 class Data():
     def __init__(self, robot):
-        self.mode = -3
+        self.mode = -4
         self.q = queue.Queue()
         
         self.robot = robot
         pc = self.robot.pointCloud
-        self.d = dict() # Will be equal to None after the scene is saved
+        self.d = dict() # Will become None after the scene is saved
         self.d['epi_starts'] = np.array([], dtype = np.bool)
         if self.mode == 0:
             self.d['observations'] = np.zeros((0, pc.hPix * pc.wPix), dtype = np.int8)
         elif self.mode == -1 or self.mode == -2:
             self.d['observations'] = np.zeros((0, pc.hPix * pc.wPix), dtype = np.int8)
             self.d['observations2'] = np.zeros((0, 1), dtype = np.float32)
-        elif self.mode == -3:
+        elif self.mode == -3 or self.mode == -4:
             self.d['observations'] = np.zeros((0, pc.hPix * pc.wPix), dtype = np.int8)
             self.d['observations2'] = np.zeros((0, 2), dtype = np.float32)
         elif self.mode > 0:
             self.d['observations'] = np.zeros((0, pc.hPix * pc.wPix * 2), dtype = np.int8)
             self.d['observations2'] = np.zeros((0, 2), dtype = np.float32)
         self.d['observations1'] = np.zeros((0, pc.lenScanVector), dtype = np.float32)
-        self.d['obs2'] = np.zeros((0, 11), dtype = np.float32)
+        self.d['obs2'] = np.zeros((0, 17), dtype = np.float32)
         self.d['actions'] = np.zeros((0, 2), dtype = np.float32)
     def getObservation(self, mode):
         # This function can not run after scene has been saved as a pickle file
@@ -51,15 +51,17 @@ class Data():
             self.q.put((obs0, act0))
         elif mode < 0:
             obs0 = self.robot.pointCloud.getObservation()
-            if mode == -1:
-                leaderXi = self.robot.leader.xi
-                vd = np.array([[(leaderXi.vx**2 + leaderXi.vy**2)**0.5]])
-            elif mode == -2:
+            if mode == -1: # 1x1 Leader's actual speed
+                vLeader = self.robot.leader.getV1V2()
+                vd = np.array([[0.5*(vLeader[0, 0] + vLeader[0, 1])]])
+            elif mode == -2: # 1x1 Follower's reference speed
                 followerXid = self.robot.xid
                 vd = np.array([[(followerXid.vx**2 + followerXid.vy**2)**0.5]])
-            elif mode == -3:
+            elif mode == -3: # 1x2 Follower's reference speed
                 followerXid = self.robot.xid
                 vd = np.array([[followerXid.vx, followerXid.vy]])
+            elif mode == -4: # 1x2 Velocities of the leader's two wheels
+                vd = self.robot.leader.getV1V2()
             ret = (obs0, vd)
         
         return ret
@@ -87,12 +89,17 @@ class Data():
         followerXi = self.robot.xi
         leaderXi = self.robot.leader.xi
         followerXid = self.robot.xid
+        vLeader = self.robot.leader.getV1V2()
         obs2Data = [[leaderXi.x, leaderXi.y, # 1, 2
                     followerXi.x, followerXi.y,  # 3, 4
                     leaderXi.x - followerXi.x, leaderXi.y - followerXi.y, # 5, 6
                     followerXid.vx, followerXid.vy, # 7, 8
                     leaderXi.theta, followerXi.theta, # 9, 10
-                    leaderXi.theta - followerXi.theta # 11
+                    leaderXi.theta - followerXi.theta, # 11
+                    0.5*(vLeader[0, 0] + vLeader[0, 1]), #12 : mode = -1
+                    (followerXid.vx**2 + followerXid.vy**2)**0.5, #13: mode = -2
+                    followerXid.vx, followerXid.vy, #14, 15: mode = -3
+                    vLeader[0, 0], vLeader[0, 1] #16, 17: mode = -3
                     ]]
         self.d['obs2'] = np.append(self.d['obs2'], obs2Data, axis = 0)
         self.d['actions'] = np.append(self.d['actions'], 
